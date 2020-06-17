b4eb9f58a18445fdec8dd3751781098e1b9c0e4d
Kalinga Dissanayake <kalingakbd@gmail.com>
2020-06-16 18:19:05 -0700

    Updates

---
+++ precache-manifest.0b27cc89973daeb38ae5e447d33122fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad10d3412d3f907d0b61156ba393a1ed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c517dc948cb91f6b2e4a",
+    "url": "/monsters-rolodex/static/css/main.b63aced7.chunk.css"
+  },
+  {
+    "revision": "d6e4ecd5ab8b0e82d993",
+    "url": "/monsters-rolodex/static/js/2.d5e9deb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d5e9deb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c517dc948cb91f6b2e4a",
+    "url": "/monsters-rolodex/static/js/main.77c99eaf.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
